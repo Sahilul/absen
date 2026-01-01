a432eb64e97481ca0fc023a341e0049af0817978
Sahilul <turuae1@gmail.com>
2026-01-01 21:52:07 +0700

    Fix: Add petugas_buku_tamu to guru_fungsi ENUM column

--- migrations/1.5.0.sql
+++ migrations/1.5.0.sql
@@ -8,3 +8,6 @@ ALTER TABLE buku_tamu ADD COLUMN IF NOT EXISTS catatan TEXT NULL AFTER bertemu_d
 UPDATE buku_tamu 
 SET foto_url = REPLACE(foto_url, 'https://drive.google.com/uc?id=', 'https://lh3.googleusercontent.com/d/') 
 WHERE foto_url LIKE '%drive.google.com/uc%';
+
+-- Add petugas_buku_tamu to guru_fungsi enum
+ALTER TABLE guru_fungsi MODIFY COLUMN fungsi ENUM('bendahara','petugas_psb','admin_cms','kurikulum','kesiswaan','petugas_buku_tamu') NOT NULL;
