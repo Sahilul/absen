2f54d5cea736785c205170b2590863caf5887104
Sahilul <turuae1@gmail.com>
2026-01-01 21:24:10 +0700

    v1.5.0: Buku Tamu enhancements - responsive views, waktu datang/pulang, catatan, download PDF

---
+++ migrations/1.5.0.sql
@@ -0,0 +1,10 @@
+-- Migration 1.5.0: Buku Tamu Enhancements
+-- Date: 2026-01-01
+
+-- Add catatan column to buku_tamu table
+ALTER TABLE buku_tamu ADD COLUMN IF NOT EXISTS catatan TEXT NULL AFTER bertemu_dengan;
+
+-- Update foto_url to use new Google Drive format (lh3.googleusercontent.com)
+UPDATE buku_tamu 
+SET foto_url = REPLACE(foto_url, 'https://drive.google.com/uc?id=', 'https://lh3.googleusercontent.com/d/') 
+WHERE foto_url LIKE '%drive.google.com/uc%';
